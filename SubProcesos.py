15128e5ad07a4ffc47904413db9920495ae0e870
AquilesIMM <aquilesmartinez490@gmail.com>
2024-05-15 01:16:44 -0300

    actualizacion sub procesos

--- SubProcesos.py
+++ SubProcesos.py
@@ -1,6 +1,17 @@
 from Gestor_de_tareas import *
-Class CRUD(tarea):
+class CRUD():
 
-    def ver_tareas(tarea):
-        
+    def ver_tareas(self, tarea):
+        for llave, valor in self.tarea.items():
+            print(valor)
 
+    def ver_llave_tarea(self, tarea):
+        for llave, valor in self.tarea.items():
+            print(llave, valor)
+    def eliminar_tarea(self, tarea):
+        print(CRUD.ver_llave_tarea(self.tarea))
+        opcion = int(input('Elija la tarea que quiere eliminar:'))
+        opcion2 = int(input(f'Seguro de eliminar: {tarea.get(opcion)}'
+              f'\n 1.si'
+              f'\n 2.no'))
+            
