9cec0e3ab50100d726df315d3e5fc285d2707d4e
Hiro3ds <vegagerardo743@gmail.com>
2024-05-15 01:08:50 -0300

    cambio_menu

--- Gestor_de_tareas.py
+++ Gestor_de_tareas.py
@@ -1,15 +1,24 @@
+opcion=None
+while opcion !=5:
+      print(f'Bienvenido a su gestor de tareas'
+      '   \n Menu'
+      ' \n1-Ver tareas'
+      ' \n2-Añadir tareas'
+      ' \n3-Editar tareas' 
+      ' \n4-Eliminar tareas'
+      ' \n5-Salir')
 
-print(f'    Menu'
-      ' 1-Ver tareas'
-      ' 2-Edita tareas'
-      ' 3-Salir')
-opcion=int(input('Ingrese la opcion que desee'))
-Tarea = ["", ""]
-if(opcion==1):
-      print()
-elif(opcion==2):
-      prit()
-elif(opcion==3):
-      print()
-else:
-      print("la opcion seleccionada no es correcta")
+      Tarea = ["", ""]
+      opcion=int(input('Ingrese la opcion que desee: '))
+      if(opcion==1):
+            print()
+      elif(opcion==2):
+            print()
+      elif(opcion==3):
+            print()
+      elif(opcion==4):
+            print()
+      elif(opcion==5):
+            print('Gracias por utilizar el gestor de tareas,vuelva prontos')
+      else:
+            print("la opcion seleccionada no es correcta")
