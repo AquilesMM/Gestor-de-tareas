c1656453e1d3a6b28e4811e2df575c5c7f6cf65b
AquilesIMM <aquilesmartinez490@gmail.com>
2024-05-15 00:27:58 -0300

    creacion de archivos

---
+++ Gestor_de_tareas.py
@@ -0,0 +1,15 @@
+
+print(f'    Menu'
+      ' 1-Ver tareas'
+      ' 2-Edita tareas'
+      ' 3-Salir')
+opcion=int(input('Ingrese la opcion que desee'))
+Tarea = ["", ""]
+if(opcion==1):
+      print()
+elif(opcion==2):
+      prit()
+elif(opcion==3):
+      print()
+else:
+      print("la opcion seleccionada no es correcta")
---
+++ SubProcesos.py
@@ -0,0 +1,6 @@
+from Gestor_de_tareas import *
+Class CRUD(tarea):
+
+    def ver_tareas(tarea):
+        
+
